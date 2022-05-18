32d1edfd3289cc6d8c4fdbe294e5672db8ebaeec
HopeZucco <hmz34@drexel.edu>
2022-05-18 15:22:14 -0400

    Code added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,47 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+//Gsap Register Plugins
+
+gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin (MorphSVGPlugin);
+gsap.registerPlugin (DrawSVGPlugin);
+gsap.registerPlugin (MotionPathHelper);
+
+const mainTL = gsap.timeline();
+
+ mainTL
+ .from (".swirl", {duration:.75,xPercent:-50, rotation:360, alpha:0})
+ //.fromTo(".swirl", {duration: 3, drawSVG:0})
+ 
+//  function setStage(){
+//     let tl = gsap.timeline();
+
+//     tl.set(".swirl", {x:"-=175", alpha:0})
+//     tl.set(".side", {x:"-=175", alpha:0})
+//     tl.set(".handle", {x:"-=175", alpha:0})
+
+//     ;
+
+
+//     return tl;
+
+    
+//     }
+
+ mainTL.add()
+
+
+ ;
+
+
+
+
+
+GSDevTools.create()
+
+
+;
